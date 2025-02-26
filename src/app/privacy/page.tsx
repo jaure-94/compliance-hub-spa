f2cba54b4a0d5e81365fd4b32a77ba816f4b327e
Tawanda Jaure <tawandajaujau@gmail.com>
2025-02-26 11:49:30 +0200

    create privacy policy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+type Props = {}
+
+const page: FC = (props: Props) => {
+  return (
+    <main className="container">
+      <div className="flex flex-col py-10">
+        <h1 className="text-2xl font-bold">Privacy Policy</h1>
+      </div>
+    </main>
+  )
+}
+
+export default page
--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -4,8 +4,7 @@ import { FaPhoneSquareAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 export const ContactCard = ({ phone, email }: { phone: string, email: string }) => {
-  return (
-    <div className='flex flex-col items-center sm:mt-10 lg:mt-0 h-fit p-5 md:px-10 md:pt-5 md:pb-10 bg-gray-100 border-[3px] border-solid border-gray-400 rounded-md shadow-[0+4px_14px_#9ca3af]'>
+  return (    <div className='flex flex-col items-center sm:mt-10 lg:mt-0 h-fit p-5 md:px-10 md:pt-5 md:pb-10 bg-gray-100 border-[3px] border-solid border-gray-400 rounded-md shadow-[0+4px_14px_#9ca3af]'>
       <div className="mt-5 flex flex-col gap-2">
         <div className="flex flex-row items-center justify-center gap-2">
           <FaPhoneSquareAlt className="h-5 w-5" />
